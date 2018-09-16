3a8ce95cb5cbb64ba92429ca4ecab4e606af5740
Sai Sameer Pusapaty <pusapaty@mit.edu>
2018-09-15 23:52:41 -0400

    creates data

--- vectorize.py
+++ vectorize.py
@@ -1,15 +1,14 @@
 from datetime import datetime
 import json
-import requests
 import subprocess
+import requests
 import numpy as np
-
 
 def process_json(filename):
     with open(filename) as f:
         data = json.load(f)
     caption = data["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
-    day, hour = convert_time(data["node"]["taken_at_timestamp"], caption)
+    day, hour = convert_time(data["node"]["taken_at_timestamp"])
     liked_by = data["node"]["edge_liked_by"]["count"]
     followers = int(get_followers(data["node"]["owner"]["id"]))
     return get_feature_vector(
@@ -17,30 +16,18 @@ def process_json(filename):
             day,
             hour,
             followers,
-            liked_by
+            liked_by/followers
         )
 
 
-def convert_time(time, caption):
+def convert_time(time):
     days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    hashtags = ['#nycselfie', '#laselfie', '#bostonselfie', '#londonselfie', '#parisselfie', '#caliselfie', '#chicagoselfie']
-    timezone_change = [-4, -7, -4, 1, 2, -7, -5]
     week_day = datetime.utcfromtimestamp(time).strftime('%A')
     hour_day = int(datetime.utcfromtimestamp(time).strftime('%H'))
 
-    index_timezone = 0
-    for i in range(len(hashtags)):
-        if hashtags[i] in caption:
-            index_timezone = i
-            break
-
-    change_day = (hour_day + timezone_change[index_timezone])//24
-    hour_day = (hour_day + timezone_change[index_timezone]) % 24
     vector_day = []
-    day_index = (days.index(week_day) + change_day) % 7
-
-    for i in range(7):
-        if i == day_index:
+    for i in days:
+        if i == week_day:
             vector_day.append(1)
         else:
             vector_day.append(0)
@@ -54,6 +41,7 @@ def get_followers(user_id):
     follower_count = requests.get("https://www.instagram.com/web/search/topsearch/?query={" + username + "}").json()["users"][0]["user"]["follower_count"]
     return follower_count
 
+def get_feature_vector(caption, day, hour, followers, liked_by_perc):
+    return np.array([followers, hour, *day, liked_by_perc], dtype=np.float32) 
+    
 
-def get_feature_vector(caption, day, hour, followers, liked_by):
-    return np.array([followers, hour, *day, liked_by], dtype=np.float32) 
