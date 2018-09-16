3445d3e769a439ad2d25eaa1164ca7462352667c
Shreyas Balaji <shreyasb99@gmail.com>
2018-09-15 21:40:38 -0400

    Refactored some code into two separate files to make things clearer

---
+++ vectorize.py
@@ -0,0 +1,35 @@
+from datetime import datetime
+import json
+
+def process_json(filename):
+    with open(filename) as f:
+        data = json.load(f)
+    caption = data["node"]["edge_media_to_caption"]["edges"][0]["node"]["text"]
+    day, hour = convert_time(data["node"]["taken_at_timestamp"])
+    liked_by = data["node"]["edge_liked_by"]["count"]
+    followers = get_followers(data["node"]["owner"]["id"])
+    return [caption, day, hour, liked_by, followers]
+
+
+def convert_time(time):
+    days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    week_day = datetime.utcfromtimestamp(time).strftime('%A')
+    hour_day = int(datetime.utcfromtimestamp(time).strftime('%H'))
+
+    vector_day = []
+    for i in days:
+        if i == week_day:
+            vector_day.append(1)
+        else:
+            vector_day.append(0)
+
+    return vector_day, hour_day
+
+
+def get_followers(user_id):
+    cmd = "curl -s https://i.instagram.com/api/v1/users/" + user_id + "/info/ | jq -r .user.username"
+    username = subprocess.check_output(cmd, shell=True).decode("utf-8")[:-1]
+    follower_count = requests.get("https://www.instagram.com/web/search/topsearch/?query={" + username + "}").json()["users"][0]["user"]["follower_count"]
+    return follower_count
+
+# print(process_json("eeifshemaisrael/2017-04-11_22-21-37_UTC.json"))
