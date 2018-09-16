6268548d2c789267c3b754b763f37fe1b180408c
Shreyas Balaji <shreyasb99@gmail.com>
2018-09-15 20:11:02 -0400

    good-row-output

--- makerow.py
+++ makerow.py
@@ -1,12 +1,74 @@
+import os
 import numpy as np
+# import tensorflow as tf
+import json
+from multiprocessing import Pool
+from collections import namedtuple
 from PIL import Image
 
 
-def load_image(filename):
-    img = Image.open(filename)
-    img.load()
-    data = np.asarray(img, dtype="int32")
-    np.pad(data, ((0, 640 - data.shape[0]), (0, 640 - data.shape[1])), 'constant', constant_values=(0, 0))
-    return data
+MAX_SIZE = 1000
+root_dir = os.path.dirname(__file__)
+SerializeInfo = namedtuple('SerializeInfo', ['input_file', 'output_file'])
 
-print(load_image("eeifshemaisrael/2017-04-11_22-23-03_UTC.jpg"))
+
+def serialize_image(info):
+    try:
+        img = Image.open(info.input_file)
+        img = img.resize((300, 300), Image.ANTIALIAS)
+        img.save(info.output_file, "JPEG")
+    except IOError:
+        print("Could not resize image")
+    return None
+
+
+def serialize_images(input_dir, output_dir):
+    """ meta filename = 'meta.json' """
+
+    odirs = os.listdir(output_dir)
+    odirs = list(filter(lambda d: d[:3] == 'tff', odirs))
+    odirs.sort()
+
+    if len(odirs) == 0:
+        odir = os.path.join(output_dir, 'tff0001')
+        os.makedirs(odir)
+    else:
+        odir = os.path.join(output_dir, odirs[-1])
+
+    if not os.path.exists(os.path.join(odir, 'meta.json')):
+        with open(os.path.join(odir, 'meta.json'), 'w') as f:
+            json.dump({'size': 0}, f)
+
+    fnames = list(os.listdir(input_dir))
+    infos = []
+    for fname in fnames:
+        if fname[-4:] == '.jpg':
+            # print(fname)
+            with open(os.path.join(odir, 'meta.json'), 'r') as f:
+                csize = json.load(f)['size']
+            if csize >= MAX_SIZE:
+                current_id = int(fname[:4].lstrip('0'))
+                fid = "%04d" % (current_id + 1)
+                odir = os.path.join(output_dir, f'tff{fid}')
+                if not os.path.exists(odir):
+                    os.makedirs(odir)
+                if not os.path.exists(os.path.join(odir, 'meta.json')):
+                    with open(os.path.join(odir, 'meta.json'), 'r') as f:
+                        json.dump({'size': 0}, f)
+                with open(os.path.join(odir, 'meta.json')) as f:
+                    csize = json.load(f)['size']
+
+            csize += 1
+            jpg_prefix = "%04d" % csize
+            infos.append(SerializeInfo(
+                os.path.join(input_dir, fname),
+                os.path.join(odir, f'{jpg_prefix}.jpg')))
+
+
+            with open(os.path.join(odir, 'meta.json'), 'w') as f:
+                json.dump({'size': csize}, f)
+
+    pool = Pool(4)
+    pool.map(serialize_image, infos)
+
+serialize_images(os.path.join(root_dir, 'eeifshemaisrael'), os.path.join(root_dir, 'data'))
